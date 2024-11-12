cadb93d6be1c150e6ecd83f10f5ea149b889d659
enzo <enzovcnt@gmail.com>
2024-11-12 23:37:21 +0100

    coords appears in the console

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-alert("Hello World!");
+const triggers = document.querySelectorAll('a');
+const highlights = document.createElement('span');
+highlights.classList.add('highlight');
+document.body.append(highlights);
+
+function highlightLink(){
+    const linkCoords = this.getBoundingClientRect();
+    console.log(linkCoords);
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
